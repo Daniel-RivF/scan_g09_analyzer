1fc00a735450e1fad7077cdcb4f5b43b5ee29715
DANIEL RIVERO <darthquevedus@gmail.com>
2015-09-30 11:08:46 +0200

    igual

--- scan_analysis.py
+++ scan_analysis.py
@@ -6,9 +6,9 @@ atom2 = int(raw_input("2nd pulling point (atom label):    "))
 type_job = int(raw_input("Type of calculation  \n (Type 1 for CASSCF and 2 for DFT):    " ))
 fileplot = raw_input(" name of the output file   :" )
 
+
 if type_job == 1:
     tools.writer_dist_F_E_CASSCF(filename,atom1,atom2,fileplot)
-    tools.
 #elif type_job == 2:
 #    tools.writer_dist_F_E_DFT(filename,atom1,atom2,fileplot)
 
